276350bc94433ad99626bc5ef27d1d57f157722e
Richard Knox <1101997k@student.gla.ac.uk>
2017-01-12 10:53:39 +0000

    Added templating

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage' : "testing"}
+
+    return render(request, 'rango/index.html', context=context_dict)
+def about(request):
+    return HttpResponse("<h1>Rango says about page!</h1><br/> <a href='/rango/'>Home</a>")
