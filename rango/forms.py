0b9b50e3f4b6537ed5cdf36f9acfef3500094da0
Richard <1101997k@student.gla.ac.uk>
2017-01-26 18:38:37 +0000

    Finished Chapter 7

--- rango/forms.py
+++ rango/forms.py
@@ -12,3 +12,13 @@ class CategoryForm(forms.ModelForm):
         #provide an association between the ModelForm and a model
         model = Category
         fields=('name',)
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="Please enter page title")
+    url = forms.URLField(max_length=200, help_text="Please enter page URL")
+    views = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+
+    class Meta:
+        model = Page
+        #what fields to include in form, easier to simply exclude category field from form
+        exclude = ('category',)
